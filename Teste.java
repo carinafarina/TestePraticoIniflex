8e0fd51b6cd4d53ec9706f10a956cfd932b3da46
Carina Farina <carinafarina2014@gmail.com>
2025-09-16 00:18:29 -0300

    first commit

---
+++ Teste.java
@@ -0,0 +1,26 @@
+package Teste;
+
+public class Pessoa{
+
+    private String nome;
+    private LocalDate dataNascimento;
+}
+public Pessoa(String nome, LocalDate dataNascimento) {
+        nome = nome;
+        dataNascimento = dataNascimento;}
+public class Funcionario extends Pessoa {
+
+    private BigDecimal salario;
+    private String funcao;
+
+}        
+public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+    super(nome, dataNascimento);
+        salario = salario;
+        funcao = funcao;
+}
+public class Principal {
+
+    public static void main(String[] args) {
+            	
+}
